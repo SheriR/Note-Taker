380631ef32b8c9ea4b1eb28f78680f9dd8912544
Sheri Rhoades <sheri_rhoades@yahoo.com>
2020-03-10 10:26:10 -0500

    add shortid to create ids

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,7 +1,7 @@
 var $noteTitle = $(".note-title");
 var $noteText = $(".note-textarea");
 var $saveNoteBtn = $(".save-note");
-var $newNoteBtn = $(".new-note");
+const $newNoteBtn = $(".new-note");
 var $noteList = $(".list-container .list-group");
 
 // activeNote is used to keep track of the note in the textarea
--- server.js
+++ server.js
@@ -3,16 +3,26 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const bodyParser = require("body-parser");
+const shortid = require("shortid");
 
 // Sets up the Express App
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -22,9 +32,46 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+app.get("/api/notes", function (req, res) {
+    return res.json(data);
+  });
+  
+  app.post("/api/notes", function (req, res) {
+    const newNote = req.body;
+  
+    data.push(newNote);
+  
+    fs.writeFile("./json/db.json", JSON.stringify(data), function (err) {
+      if (err) throw err;
+      console.log("test");
+  
+    });
+  
+    return res.json(true);
+  
+  });
+  
+  app.delete("/api/notes/:id", function (req, res) {
+    let id = req.params.id;
+    data = data.filter(function (note) {
+      if (note.id === id) {
+        return false;
+      }
+      return true;
+    });
+  
+    fs.writeFile("./json/db.json", JSON.stringify(data), function (err) {
+      if (err) throw err;
+      res.end();
+    });
+  
+    return res.json(data);
+  
+  });  
 
-
-
+app.use('*', (_, res) => {
+	res.redirect('/');
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
